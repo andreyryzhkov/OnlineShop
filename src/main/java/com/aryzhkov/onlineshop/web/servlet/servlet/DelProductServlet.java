db154cdf9de2ddd40a34fbe287f90f3fcf29bb20
andreyryzhkov <andrey.ryzhkov77@gmail.com>
2019-02-10 23:12:04 +0200

    Added session

--- src/main/java/com/aryzhkov/onlineshop/web/servlet/servlet/DelProductServlet.java
+++ src/main/java/com/aryzhkov/onlineshop/web/servlet/servlet/DelProductServlet.java
@@ -17,6 +17,11 @@ public class DelProductServlet extends HttpServlet {
     }
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int id = Integer.parseInt(req.getParameter("id"));
         productService.deleteProduct(id);
